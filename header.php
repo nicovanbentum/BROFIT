2750f39dfe8bbd7bd878e95b5e77db78fb1958cb
Nico van Bentum <nico.vanbentum@student.hu.nl>
2018-01-24 13:18:09 +0100

    header php call

---
+++ header.php
@@ -0,0 +1,12 @@
+<header>
+    <div class="navbar">
+        <a href="http://localhost/bro/brofit/Home.html">Home</a>
+        <a href="http://localhost/bro/brofit/Sportinzage.html"> Sportinzage</a>
+        <a href="http://localhost/bro/brofit/SporttipsEnAdviezen.html">Sporttips en Adviezen</a>
+        <a href="http://localhost/bro/brofit/Aanbod.html">Aanbod</a>
+        <a href="http://localhost/bro/brofit/Tarieven.html">Tarieven</a>
+        <a href="http://localhost/bro/brofit/Contactinformatie.html">Contact Informatie</a>
+        <a id="INLOG" href="http://localhost/bro/brofit/registreer.php">Registreren</a>
+        <a id="INLOG" href="http://localhost/bro/brofit/inloggen.php">Inloggen</a>
+    </div>
+</header>
--- inloggen.php
+++ inloggen.php
@@ -1,21 +1,5 @@
-<?php
-$user=" ";
-$password=" ";
-$hashed_password = password_hash($password, PASSWORD_DEFAULT);
-
-//login check
-if(isset($_POST['submit'])){
-
-    if(($_POST['user'] == $user) and (password_verify($password, $hashed_password))){
-        echo "login gelukt.";
-    }
-    else
-    {
-        echo "login mislukt.";
-    }
-}
-?>
 <html>
+
 <head>
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
@@ -25,19 +9,9 @@ if(isset($_POST['submit'])){
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 </head>
 
-<header>
-    <div class="navbar">
-        <a href="http://localhost/SportschoolBenno/Home.html">Home</a>
-        <a href="http://localhost/SportschoolBenno/Sportinzage.html"> Sportinzage</a>
-        <a href="http://localhost/SportschoolBenno/SporttipsEnAdviezen.html">Sporttips en Adviezen</a>
-        <a href="http://localhost/sportschoolBenno/Aanbod.html">Aanbod</a>
-        <a href="http://localhost/sportschoolBenno/Tarieven.html">Tarieven</a>
-        <a href="http://localhost/sportschoolBenno/Contactinformatie.html">Contact Informatie</a>
-        <a id="INLOG" href="http://localhost/sportschoolBenno/registreer.php">Registreren</a>
-        <a id="INLOG" href="http://localhost/sportschoolBenno/inloggen.php">Inloggen</a>
-
-    </div>
-</header>
+<?php
+include_once("header.php");
+?>
 
 <body>
 
