83950415675912936c34a6cf976d27f4685f11ac
Nico van Bentum <nico.vanbentum@student.hu.nl>
2018-01-25 15:56:45 +0100

    w0w

--- login.php
+++ login.php
@@ -4,14 +4,12 @@ error_reporting(E_ALL);
 require_once("connect.php");
 require_once("Query.php");
 
-$wwselsql="SELECT `password` FROM `user` WHERE `username` LIKE '" . mysql_escape_string($_POST['user'])
+$gebruikersnaam=$_POST['user'];
+
+$wwselsql= "SELECT `password` FROM `user` WHERE `username` LIKE '$gebruikersnaam'";
 $selWachtwoord=Query($wwselsql, null , true, false, false);
 
-if(password_verify($selWachtwoord) == $_POST['password']){
-    echo "login gelukt";
-}
-
-if(isset($_POST['submit'])){
+if(isset($_POST['submit']) and password_verify('$selWachtwoord', PASSWORD_DEFAULT) == $_POST['password']){
     echo 'submitted';
 }
 
