df120aee3d12f7570f50d8b45b4f52677f3aec09
Nico van Bentum <nico.vanbentum@student.hu.nl>
2018-01-28 22:22:40 +0100

    NAW updating

--- updateNAW.php
+++ updateNAW.php
@@ -13,16 +13,16 @@ $selKlantid=Query($wwselsql, array('gebruikersnaam'=>$gebruikersnaam))[0];
 $naam=$_POST['naam'];
 $adres=$_POST['adres'];
 $woonplaats=$_POST['woonplaats'];
-$klantid=$_SESSION['klantid'];
+$klantid=$selKlantid;
 
 $valuesNAW = array('naam'=>$naam, 'adres'=>$adres, 'woonplaats'=>$woonplaats );
+
+$sqlNAW = "UPDATE klant SET (naam, adres, woonplaats) VALUES (:naam, :adres, :woonplaats) WHERE klantid = '$klantid'";
+
+Query($sqlNAW, $valuesNAW, false, false, true);
 
 if(isset($_POST['submit'])){
     header('Location: index.php');
 }
-
-$sqlNAW = "UPDATE klant SET (naam, adres, woonplaats) VALUES (:naam, :adres, :woonplaats) WHERE klantid = '$klantid'";
-
-Query($sqlNAW, $valuesNAW, false, false, true)
 
 ?>
